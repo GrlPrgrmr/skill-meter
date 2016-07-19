e15a1b425764c00e1996f4aa5c25b04c1ec2cc9d
Neha261289 <neha.bpit@gmail.com>
2016-07-19 05:56:12 -0700

    main application folder content

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SkillMeter.ViewModels;
+
+namespace SkillMeter
+{
+    // <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+            : this(null)
+        { }
+        public MainWindow(string vm)
+        {
+            InitializeComponent();
+          
+
+            if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+            {
+                //Code that throws the exception
+                this.DataContext = new MainWindowViewModel();
+            }
+
+        }
+    }
+}
