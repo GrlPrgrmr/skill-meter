d80593d5709c0e2b45058621740d485fbe3dcebd
Neha261289 <neha.bpit@gmail.com>
2016-07-19 05:55:34 -0700

    Views Folder Content

---
+++ AnswerSheetView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SkillMeter.Views
+{
+    /// <summary>
+    /// Interaction logic for AnswerSheetView.xaml
+    /// </summary>
+    public partial class AnswerSheetView : UserControl
+    {
+        public AnswerSheetView()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            dgrid.UnselectAllCells();
+        }
+    }
+}
---
+++ QuestionSetView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SkillMeter.Views
+{
+    /// <summary>
+    /// Interaction logic for QuestionSetView.xaml
+    /// </summary>
+    public partial class QuestionSetView : UserControl
+    {
+        public QuestionSetView()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            dGrid.UnselectAllCells();
+        }
+    }
+}
